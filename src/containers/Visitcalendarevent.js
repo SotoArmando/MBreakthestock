311b834161f079e59e71ce02b6aada01df8fc640
SotoArmando <armando29@live.com>
2021-02-24 19:51:54 -0400

    Added comment, submit sections

--- src/containers/Visitcalendarevent.js
+++ src/containers/Visitcalendarevent.js
@@ -10,5 +10,65 @@ export default function Visitcalendarevent() {
             <span className="f_1 fore_17 ls_24 f400 lh_3 mar_t30">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam.</span>
             <span className="f_1 fore_17 ls_24 f400 lh_3 mar_t30">Carried nothing on warrant towards. Polite in of in oh needed itself silent course. Whatever may scarcely had. Assistance travelling especially prosperous appearance mr no itself celebrated. Wanted easily in my called formed suffer. Songs hoped sense as taken ye mirth at. Believe fat considered projection how six drawing pursuit minutes far. Same do seen head am part it dear open to.</span>
         </div>
+        <div className="row space_between items_center corebox_5 fore_17">
+            <div className="row">
+                <div className="corebox_4 corebox_x3 back_3 center pad_l26 pad_r26 fore_14">Branding</div>
+                <div className="corebox_4 corebox_x3 back_3 center pad_l26 pad_r26 fore_14 mar_l24">Photography</div>
+            </div>
+            <div className="row">
+				<div className="corebox_3 corebox_x3 back_2 center pad_l26 pad_r26 btn ">Previous Post</div>
+                <div className="corebox_3 corebox_x3 back_2 center pad_l26 pad_r26 mar_l24 btn ">Next Post</div>
+            </div>
+        </div>
+
+        <div className="row start items_center corebox_7 fore_11 border_t3 border_b3 mar_t26 pad_t30 mobilepad_l24">
+            <div className="f_2 f500">1 Comment on “All the world's waiting for you”</div>
+        </div>
+        <div className="corebox_9 row start items_center">
+			<div className="row start items_start mobilepad_l20 mobilepad_t28">
+				<div className="bidcorebox_4 back_3"></div>
+				<div className=" col pad_l29">
+					<div className="f600 row fore_16 corebox_2">
+						Anna Watson
+						<span class="fore_14 f400 mar_l24">December 21, 2017 at 2:34 am</span>
+						<div class="fore_15 f400 btn hover mar_l24">Reply<div className="to_hover fore_11">Reply</div></div>
+					</div>
+					<div className="fore_16 f400 ls_25 pad_l28">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</div>
+				</div>
+			</div>
+		</div>
+		<div className="corebox_9 row start items_center">
+			<div className="row start items_start mobilepad_l20 mobilepad_t28">
+				<div className="bidcorebox_4 back_3"></div>
+				<div className=" col pad_l29">
+					<div className="f600 row fore_16 corebox_2">
+						Anna Watson
+						<span class="fore_14 f400 mar_l24">December 21, 2017 at 2:34 am</span>
+						<div class="fore_15 f400 btn hover mar_l24">Reply<div className="to_hover fore_11">Reply</div></div>
+					</div>
+					<div className="fore_16 f400 ls_25 pad_l28">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</div>
+				</div>
+			</div>
+		</div>
+		<div className="row start items_center corebox_7 fore_11  border_b3 mar_t26 pad_t30 mobilepad_l24">
+            <div className="f_2 f500">Leave a comment</div>
+			
+        </div>
+		<div className="col pad_l24 pad_r24 pad_t26">
+			<textarea className="corebox_10 back_3 pad_24 ls_25" type="text" placeholder="Text"/>
+			<div className="row basis_45 mar_t24 corebox_3" >
+				<input className="back_3 mar_r24 mobilemar_r0  pad_24 ls_25"  type="text" placeholder="Your Name"/>
+				<input className="back_3 pad_24 ls_25" type="text" placeholder="Your Mail"/>
+			</div>
+		</div>
+		<div className="row space_between items_center corebox_5 fore_17 ">
+            <div className="row">
+               <div className="corebox_3 corebox_x10 back_2 center pad_l26 pad_r26 mar_l24 btn ">Submit</div>
+            </div>
+            <div className="row">
+		
+                
+            </div>
+        </div>
     </div>
 }
