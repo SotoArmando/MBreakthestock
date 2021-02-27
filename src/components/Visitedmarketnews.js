084ec1c7dda77591a32e7b8dcb278f57577fa580
SotoArmando <armando29@live.com>
2021-02-27 14:12:45 -0400

    Updated visitedmarketnews to 17

--- src/components/Visitedmarketnews.js
+++ src/components/Visitedmarketnews.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export default function Visitedmarketnews({ category, datetime, headline, related, source, image, summary, url, basis }) {
 
-    return <div className="col corebox_11  center items_start  border_t4 pad_b30 pad_t30 mobilepad_l24 pad_l24 btn " style={{ flexBasis: basis + 'px', maxWidth: basis + 'px' }}>
+    return <div className="col corebox_17  center items_start  border_t4 pad_b30 pad_t30 mobilepad_l24 pad_l24 btn " style={{ flexBasis: basis + 'px', maxWidth: basis + 'px' }}>
         <a className="allsize col   space_between fore_11 pad_r15 hover_b" target='_tab' href={url}>
             <div  className="corebox_8 " style={{background:"url("+image+")",width:"100%",backgroundPosition:'center', backgroundSize:'cover'}}  />
             <div className="col    pad_b23 ">
@@ -10,7 +10,7 @@ export default function Visitedmarketnews({ category, datetime, headline, relate
                     <span className="f_0 fore_green f600 capitalize">{category}</span>
                 </div>
                 <div className="row center space_between allwidth mar_t19">
-                    <span className="f_1 f500 fore_16  "><span className="fore_green f600">{source} {new Date(datetime * 1000).toLocaleDateString()}</span> <span className="f_0 ls_27 fw400" >{headline}</span>.</span>
+                    <span className="f_1 f500 fore_16  "><span className="fore_green f600">{source} {new Date(datetime * 1000).toLocaleDateString()}</span> <span className="f_1 ls_27 f500" >{headline}</span> <br/> <span className="f_0 ls_27 fore_14 f400">{summary}</span>.</span>
                 </div>
             </div>
             <div className="to_hover_b ls_27 f_0 f500 fore_14">> Keep Reading</div>
